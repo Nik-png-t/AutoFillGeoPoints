9cf1f5e9c79ff3b95c7378d97a070f9cde476daf
Nikita <nikvoropanov74@gmail.com>
2026-09-18 14:57:01 +0300

    Add automatic create mavic flight and some fix

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ import json
 import asyncio
 from datetime import datetime, timezone, date
 import re
+from itertools import cycle
+import shutil
 # telegram
 import python_socks
 from telethon import TelegramClient, sync, utils, connection
@@ -18,13 +20,13 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 class Pack:
-    def __init__(self, name_dir=None, name_file=None, size_file=None, high=None, receiver_number=None):
-        self.name = name_dir.split("-")[0] # [ГТСП, ОП, ТСП, ТСП-ОП]
+    def __init__(self, name_dir=None, name_file=None, size_file=None, high=None, receiver_name=None):
+        self.name = name_dir.split("-")[0] # [ГТСП, ОП, ТСП]
         self.high = high
         self.name_dir = name_dir # полное название
         self.name_file = name_file # название файла .jps
         self.size_file = size_file
-        self.receiver_number = self.receiver_number
+        self.receiver_name = receiver_name
 
 class SafetyStorage:
     def __init__(self):
@@ -42,9 +44,6 @@ class SafetyStorage:
                 proxy_host = None
                 proxy_port = None
                 proxy_secret = None
-            name = None
-            path = None
-            target_date = None
             self.data = {
                 "password": password,
                 "api_id": api_id,
@@ -53,8 +52,9 @@ class SafetyStorage:
                 "proxy_host": proxy_host,
                 "proxy_port": proxy_port,
                 "proxy_secret": proxy_secret,
-                "name": name,
-                "path": path
+                "name": None,
+                "path": None,
+                "receiver_names": {} # {22: "purple", 33: "red"}
             }
             self.save_data()
             return
@@ -73,7 +73,6 @@ class SafetyStorage:
             
         except Exception as e:
             raise ValueError(f"\nОшибка расшифровки! Неверный пароль или данные повреждены. {e}")
-
         self.question_print_data()
         self.question_change_data()
         self.save_data()
@@ -101,9 +100,8 @@ class SafetyStorage:
             print(f"    Пароль: {self.data['password']}\n    API ID: {self.data['api_id']}\n    API HASH: {self.data['api_hash']}")
             print(f"    Нужен ли прокси: {self.data['is_proxy']}")
             if self.data['is_proxy']:
-                print(f"    Прокси HOST: {self.data['proxy_host']}\n    Прокси PORT: {self.data['proxy_port']}\n Проки SECRET: {self.data['proxy_secret']}")
-            
-    
+                print(f"        Прокси HOST: {self.data['proxy_host']}\n        Прокси PORT: {self.data['proxy_port']}\n        Проки SECRET: {self.data['proxy_secret']}")
+
     def question_change_data(self):
         if input("Изменить что-то: "):
             self.data['password'] = self.not_any_way_answer("Новый пароль: ", self.data['password'])
@@ -117,10 +115,30 @@ class SafetyStorage:
                 self.data['proxy_host'] = self.not_any_way_answer("Новый прокси host: ", self.data['proxy_host'])
                 self.data['proxy_port'] = self.not_any_way_answer("Новый прокси port: ", self.data['proxy_port'])
                 self.data['proxy_secret'] = self.not_any_way_answer("Новый прокси secret: ", self.data['proxy_secret'])
+            if self.data["receiver_names"]:
+                if input("Изменить номер - название приемника: "):
+                    for num, name in list(self.data["receiver_names"].items()):
+                        a = input(f"Изменить {num} - {name}: ")
+                        if a.isdigit():
+                            self.data["receiver_names"].pop(num)
+                            self.data["receiver_names"][int(a)] = name
+                        elif len(a.split(" ")) == 2:
+                            num_, name_ = a.split(" ")
+                            if num_.isdigit():
+                                self.data["receiver_names"].pop(num)
+                                self.data["receiver_names"][int(num_)] = name_
+                        elif a:
+                            self.data["receiver_names"][num] = a
         today = date.today().strftime("%Y-%m-%d")
         self.data['date'] = self.not_any_way_answer(f"Дата ({today}): ", today)
-        name = re.sub(r"\d{4}([_-]\d{2}){2}", self.data['date'], self.data['name'])
-        path = re.sub(r"\d{4}([_-]\d{2}){2}", self.data['date'], self.data['path'])
+        if self.data["name"] is not None:
+            name = re.sub(r"\d{4}([_-]\d{2}){2}", self.data['date'], self.data['name'])
+        else:
+            name = None
+        if self.data["path"] is not None:
+            path = re.sub(r"\d{4}([_-]\d{2}){2}", self.data['date'], self.data['path'])
+        else:
+            path = None
         self.data['name'] = self.not_any_way_answer(f"Имя файла xml({name}): ", name)
         self.data['path'] = self.not_any_way_answer(f"Путь до директории({path}): ", path)
         
@@ -160,10 +178,10 @@ class TelegramConnect:
             
             mtproto = (self.storage.data["proxy_host"], int(self.storage.data["proxy_port"]), self.storage.data["proxy_secret"])
             mtproto_connection = connection.tcpmtproxy.ConnectionTcpMTProxyAbridged
-            self.client = telethon.TelegramClient('session',api_id=self.storage.data["api_id"],api_hash=self.storage.data["api_hash"],proxy=mtproto,connection=mtproto_connection)
+            self.client = telethon.TelegramClient('session1',api_id=self.storage.data["api_id"],api_hash=self.storage.data["api_hash"],proxy=mtproto,connection=mtproto_connection)
 
         else:
-            self.client = TelegramClient('autofillgeo', self.storage.data["api_id"], self.storage.data["api_hash"])
+            self.client = TelegramClient('autofillgeo1', self.storage.data["api_id"], self.storage.data["api_hash"])
 
         with self.client:
             self.client.loop.run_until_complete(self.start_client())
@@ -174,7 +192,13 @@ class TelegramConnect:
         me = await self.client.get_me()
         print(f'Привет, {me.first_name}! Соединение успешно установлено.')
 
-        
+    def change_rec_num_to_file(self, number):
+        if number not in self.storage.data["receiver_names"].keys() and str(number) not in self.storage.data["receiver_names"].keys():
+            self.storage.data["receiver_names"][number] = input(f"Название файлов приемника номером {number}(purple, red, etc): ")
+            self.storage.save_data()
+        return self.storage.data["receiver_names"][str(number)]
+
+
 
     async def find_message(self):
         try:
@@ -190,19 +214,19 @@ class TelegramConnect:
             if message.date < start_of_day:
                 break
             text = message.text.split(" ")
-            if text[-1].isdigit() and text[0].split("-")[0] in ["ТСП-ОП", "ОП", "ГТСП"] and message.photo:
+            if text[-1].isdigit() and text[0].split("-")[0] in ["ТСП", "ОП", "ГТСП"] and message.photo:
                 if text[-2].isdigit():
-                    receiver_number = int(text[-1])
                     name_point = " ".join(text[:-2])
                     high_point = int(text[-2])
+                    receiver_name = self.change_rec_num_to_file(int(text[-1]))
                 else:
                     name_point = " ".join(text[:-1])
                     high_point = int(text[-1])
-                    receiver_number = None
-                print(f"Найдено имя: {name_point}, высота {high_point}, номер приемника {receiver_number}")
+                    receiver_name = None
+                print(f"Найдено имя: {name_point}, высота {high_point}, название приемника {receiver_name}")
                 full_path =  f"{self.storage.data['path']}\\{name_point}"
                 os.makedirs(full_path, exist_ok=True)
-                self.packed.append(Pack(name_dir=name_point, high=high_point, receiver_number=receiver_number))
+                self.packed.append(Pack(name_dir=name_point, high=high_point, receiver_name=receiver_name))
                 idx = 1
                 async for msg in self.client.iter_messages('me', limit=60, min_id=message.id - 30, max_id=message.id + 30):
                     full_path_img = os.path.join(full_path, f"{idx}.jpg")
@@ -264,7 +288,7 @@ class AutoXML:
             self.full_copy_paste(copy_, paste_)
         
 
-    def create_XML(self, list_info):
+    def create_XML(self, list_info, path, name_file):
         wb = load_workbook(f"{self.source_path}\\pattern.xlsx")
         ws = wb.active  
         ws.title = "Данные координирования"
@@ -273,6 +297,7 @@ class AutoXML:
         
         self.full_copy_paste(ws["A5"], ws[f"A{number_of_end_string}"])
         self.full_copy_paste(ws["B5"], ws[f"B{number_of_end_string}"])
+        ws[f"B{number_of_end_string}"] = input("Комментарий к файлу: ")
         ws.unmerge_cells("B5:I5") 
         ws.delete_rows(idx=5)
         ws.merge_cells(f"B{number_of_end_string-1}:I{number_of_end_string-1}")
@@ -283,7 +308,7 @@ class AutoXML:
             self.fill_string(ws, 4+i, info)
             
 
-        wb.save(f"{self.storage.data['path']}\\{self.storage.data['name']}.xlsx")
+        wb.save(f"{path}\\{name_file}.xlsx")
 
     def fill_string(self, ws, number_string, string):
         ws[f'A{number_string}'] = string.name
@@ -295,7 +320,20 @@ class AutoXML:
     def run(self):
         input("Можно создать XML файл? ")
         self.search_into_directory(self.storage.data["path"])
-        self.create_XML(self.tg.packed)
+        self.create_XML(self.tg.packed, self.storage.data['path'], self.storage.data['name'])
+        if input("Нужно создать XML для полетов мавика?: "):
+            list_packed_for_flight = [pack for pack in self.tg.packed if pack.name in ["ГТСП", "ТСП"]]
+            name_directory = self.storage.data['date'] + " " + ", ".join(set(re.findall(r'\d+', pack.name_dir)[0] for pack in list_packed_for_flight if pack.name=="ТСП"))
+            path_for_flight = f"{'\\'.join(self.storage.data['path'].split('\\')[:-1])}\\{name_directory}"
+            try:
+                shutil.copytree(self.storage.data['path'], path_for_flight)
+            except OSError:
+                print("Копирование не будет выполнено папка уже существует")
+            for n in os.listdir(path_for_flight):
+                if os.path.isdir(f'{path_for_flight}\\{n}') and "ТСП-ОП" not in n and "ГТСП" not in n:
+                    shutil.rmtree(f'{path_for_flight}\\{n}')
+
+            self.create_XML(list_packed_for_flight, path_for_flight, f"{self.storage.data['date']}_ТСП_под_полёты")
 
         
 def folder_distribution_of_files(path_to_files, path_to_folders, list_info):
@@ -329,74 +367,68 @@ def folder_distribution_of_files(path_to_files, path_to_folders, list_info):
     if len(folder_bases):
         #folder_base = [ГТСП-2, ГТСП-3 Ульт-Ягун]
         #base_files = [purple, [23_purple.jps]), (white, [white.jps])]
-        #list_info = [.name_dir=ГТСП-2, .receiver_number=12]
-        base_files = {{file_name: files} for file_name, files in count_of_type_jps_files.items() if len(files) == 1}
+        #list_info = [.name_dir=ГТСП-2, .receiver_name=12]
+        base_files = {file_name: files for file_name, files in count_of_type_jps_files.items() if len(files) == 1}
         for info in list_info:
-            if info.name_dir in folder_bases and info.receiver_number is not None:
-                base_file_name = change_rec_num_to_file(info.receiver_number) # purple, white, black
+            if info.name_dir in folder_bases and info.receiver_name is not None:
+                base_file_name = info.receiver_name
                 if base_file_name in base_files.keys():
                     base_file = base_files[base_file_name][0]
                 else:
-                    raise ValueError("Указаный {base_file_name} not in {base_files.keys()}")
+                    raise ValueError(f"Указаный {base_file_name} not in {base_files.keys()}")
                 print(f"Найдена база {info.name_dir} = {base_file}")
                 os.rename(f"{path_to_files}\\{base_file}", f"{path_to_folders}\\{info.name_dir}\\{base_file}")
-                folder_bases.pop(info.name_dir)
+                folder_bases.remove(info.name_dir)
                 base_files.pop(base_file_name)
                 count_of_type_jps_files.pop(base_file_name)
                 
-        if len(base_files) == 1:
+        if len(list(base_files)) == 1:
             file_name = base_files.keys()[0]
             file = base_files[file_name][0]
             print(f"Найдена база {folder_bases[0]} = {file}")
             count_of_type_jps_files.pop(file_name)
             os.rename(f"{path_to_files}\\{file}", f"{path_to_folders}\\{folder_bases[0]}\\{file}")
+
+
+    # распределяем файлов
+    queue_files = [info.receiver_name for info in list_info if info.name != "ГТСП"][::-1]
+    jps_files_name_iter = cycle(count_of_type_jps_files.keys())
+
+
+    if len(count_of_type_jps_files.keys()) == 2 and None in queue_files:
+        type_ = input(
+            f"Какой тип двойной сортировки выберите первый файл (1 вариант: {next(jps_files_name_iter)}, 2 вариант: {next(jps_files_name_iter)}): ")
+        if type_ == "2":
+            jps_files_name_iter = cycle(list(count_of_type_jps_files.keys())[::-1])
+    elif len(count_of_type_jps_files.keys()) == 0:
+        print("Файлы не найдены.Распределения не будет!")
+        return
+
+    skip_ = False
+    for i, value in enumerate(queue_files):
+        # fill None cell
+        if value is None:
+            if skip_:
+                next(jps_files_name_iter)
+                skip_ = False
+            value = next(jps_files_name_iter)
         else:
-            input("Не получилось переместить файлы. Сделайте самостоятельно")
+            skip_ = True
 
+        if value in count_of_type_jps_files.keys() and not len(count_of_type_jps_files[value]):
+            count_of_type_jps_files.pop(value)
+            if not len(count_of_type_jps_files.keys()):
+                print("Не хватило файлов для полного распределения!!!")
+                queue_files = queue_files[:i]
+                break
+        queue_files[i] = count_of_type_jps_files[value].pop(0)
 
-    # распределяем помеченые файлы
-    if
-    # -> queue = [None, purple, purple, None, None, None]
-    # распределяем остальные файлы
-    file_names = list(count_of_type_jps_files.keys())
-    if len(file_names) == 1:
-        print("Применена одиночная сортировка")
-        files = count_of_type_jps_files[file_names[0]]
-        for pack_info in list_info:
-            if pack_info.name_dir in empty_folder:
-                if len(files):
-                    print(files[0], pack_info.name_dir)
-                    os.rename(f"{path_to_files}\\{files[0]}", f"{path_to_folders}\\{pack_info.name_dir}\\{files[0]}")
-                    files.pop(0)
-    elif len(file_names) == 2:
-        
-        type_ = input(f"Какой тип двойной сортировки выберите первый файл (1 вариант: {file_names[0]}, 2 вариант: {file_names[1]}): ")
-        if not type_:
-            type_ = 0
-        else:
-            type_ = int(type_)-1
-
-        queue_files = [None for i in range(len(empty_folder))]
-        files = []
-        for i in range(len(count_of_type_jps_files[file_names[0]])):
-            if type_:
-                x = -1
-            else:
-                x = 0
-            files.append(count_of_type_jps_files[file_names[x]][i])
-            files.append(count_of_type_jps_files[file_names[x+1]][i])
-        for i, q in enumerate(queue_files):
-            if q is None:
-                queue_files[i] = files.pop(0)
-                
-        for pack_info in list_info[::-1]:
-            if pack_info.name_dir in empty_folder:
-                if len(files):
-                    print(files[0], pack_info.name_dir)
-                    os.rename(f"{path_to_files}\\{files[0]}", f"{path_to_folders}\\{pack_info.name_dir}\\{files[0]}")
-                    files.pop(0)
-
-        
+    for pack_info in list_info[::-1]:
+        if pack_info.name_dir in empty_folder:
+            if len(queue_files):
+                file = queue_files.pop(0)
+                print(file, pack_info.name_dir)
+                os.rename(f"{path_to_files}\\{file}", f"{path_to_folders}\\{pack_info.name_dir}\\{file}")
 
 
 if __name__ == "__main__":
